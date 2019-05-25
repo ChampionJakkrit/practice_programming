329032f925f317a9d5f04c7267027b57f56db17d
Jakkrit Boonnet <dekthefest_29@hotmail.com>
2019-05-25 16:53:47 +0700

    delete comment in file with constant of PI

--- (0006) Herman.c
+++ (0006) Herman.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 #define PI acos(-1.0)
-// #define PI 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170
-// #define PI 4 * atan(1)
 int main()
 {
     double radius, circle_area, taxi_area;
