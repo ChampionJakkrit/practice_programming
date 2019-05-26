608f787fb53fc5adc0289538f8e304abd7fdc6b4
Jakkrit Boonnet <dekthefest_29@hotmail.com>
2019-05-26 14:29:09 +0700

    edit 0015

--- (0015) Skocimis.cpp
+++ (0015) Skocimis.cpp
@@ -1,15 +1,38 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, c;
+    int a, b, c, count = 0;
     scanf("%d %d %d", &a, &b, &c);
     int dif1 = b-a, dif2 = c-b;
     if (dif2 > dif1) {
-        printf("%d\n", dif2-1);
+        while (!(a == c || b == c)) {
+            a = b + 1;
+            if (a != c)
+                count++;
+            else
+                break;
+            b = a + 1;
+            if (b != c)
+                count++;
+            else
+                break;
+        }
     }
     else {
-        printf("%d\n", dif1-1);
+        while (!(b == a|| c == a)) {
+            c = b - 1;
+            if (c != a)
+                count++;
+            else
+                break;
+            b = c - 1;
+            if (b != a)
+                count++;
+            else
+                break;
+        }
     }
+    printf("%d\n", count);
 
     return 0;
 }
