c77b649b34b2028b52567d7cd9a31fdab26db01c
Jakkrit Boonnet <dekthefest_29@hotmail.com>
2019-06-19 16:55:11 +0700

    do task 0023

---
+++ (0023) DATUM.cpp
@@ -0,0 +1,237 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int D, M;
+    scanf("%d%d", &D, &M);
+    if (M == 1) {
+        int day = D % 7;
+        if (day == 1)
+            printf("Thursday\n");
+        else if (day == 2)
+            printf("Friday\n");
+        else if (day == 3)
+            printf("Saturday\n");
+        else if (day == 4)
+            printf("Sunday\n");
+        else if (day == 5)
+            printf("Monday\n");
+        else if (day == 6)
+            printf("Tuesday\n");
+        else if (day == 7)
+            printf("Wednesday\n");
+        else
+            printf("Wednesday\n");
+    }
+    else if (M == 2) {
+        int day = D % 7;
+        if (day == 1)
+            printf("Sunday\n");
+        else if (day == 2)
+            printf("Monday\n");
+        else if (day == 3)
+            printf("Tuesday\n");
+        else if (day == 4)
+            printf("Wednesday\n");
+        else if (day == 5)
+            printf("Thursday\n");
+        else if (day == 6)
+            printf("Friday\n");
+        else if (day == 7)
+            printf("Saturday\n");
+        else
+            printf("Saturday\n");
+    }
+    else if (M == 3) {
+        int day = D % 7;
+        if (day == 1)
+            printf("Sunday\n");
+        else if (day == 2)
+            printf("Monday\n");
+        else if (day == 3)
+            printf("Tuesday\n");
+        else if (day == 4)
+            printf("Wednesday\n");
+        else if (day == 5)
+            printf("Thursday\n");
+        else if (day == 6)
+            printf("Friday\n");
+        else if (day == 7)
+            printf("Saturday\n");
+        else
+            printf("Saturday\n");
+    }
+    else if (M == 4) {
+        int day = D % 7;
+        if (day == 1)
+            printf("Wednesday\n");
+        else if (day == 2)
+            printf("Thursday\n");
+        else if (day == 3)
+            printf("Friday\n");
+        else if (day == 4)
+            printf("Saturday\n");
+        else if (day == 5)
+            printf("Sunday\n");
+        else if (day == 6)
+            printf("Monday\n");
+        else if (day == 7)
+            printf("Tuesday\n");
+        else
+            printf("Tuesday\n");
+    }
+    else if (M == 5) {
+        int day = D % 7;
+        if (day == 1)
+            printf("Friday\n");
+        else if (day == 2)
+            printf("Saturday\n");
+        else if (day == 3)
+            printf("Sunday\n");
+        else if (day == 4)
+            printf("Monday\n");
+        else if (day == 5)
+            printf("Tuesday\n");
+        else if (day == 6)
+            printf("Wednesday\n");
+        else if (day == 7)
+            printf("Thursday\n");
+        else
+            printf("Thursday\n");
+    }
+    else if (M == 6) {
+        int day = D % 7;
+        if (day == 1)
+            printf("Monday\n");
+        else if (day == 2)
+            printf("Tuesday\n");
+        else if (day == 3)
+            printf("Wednesday\n");
+        else if (day == 4)
+            printf("Thursday\n");
+        else if (day == 5)
+            printf("Friday\n");
+        else if (day == 6)
+            printf("Saturday\n");
+        else if (day == 7)
+            printf("Sunday\n");
+        else
+            printf("Sunday\n");
+    }
+    else if (M == 7) {
+        int day = D % 7;
+        if (day == 1)
+            printf("Wednesday\n");
+        else if (day == 2)
+            printf("Thursday\n");
+        else if (day == 3)
+            printf("Friday\n");
+        else if (day == 4)
+            printf("Saturday\n");
+        else if (day == 5)
+            printf("Sunday\n");
+        else if (day == 6)
+            printf("Monday\n");
+        else if (day == 7)
+            printf("Tuesday\n");
+        else
+            printf("Tuesday\n");
+    }
+    else if (M == 8) {
+        int day = D % 7;
+        if (day == 1)
+            printf("Saturday\n");
+        else if (day == 2)
+            printf("Sunday\n");
+        else if (day == 3)
+            printf("Monday\n");
+        else if (day == 4)
+            printf("Tuesday\n");
+        else if (day == 5)
+            printf("Wednesday\n");
+        else if (day == 6)
+            printf("Thursday\n");
+        else if (day == 7)
+            printf("Friday\n");
+        else
+            printf("Friday\n");
+    }
+    else if (M == 9) {
+        int day = D % 7;
+        if (day == 1)
+            printf("Tuesday\n");
+        else if (day == 2)
+            printf("Wednesday\n");
+        else if (day == 3)
+            printf("Thursday\n");
+        else if (day == 4)
+            printf("Friday\n");
+        else if (day == 5)
+            printf("Saturday\n");
+        else if (day == 6)
+            printf("Sunday\n");
+        else if (day == 7)
+            printf("Monday\n");
+        else
+            printf("Monday\n");
+    }
+    else if (M == 10) {
+        int day = D % 7;
+        if (day == 1)
+            printf("Thursday\n");
+        else if (day == 2)
+            printf("Friday\n");
+        else if (day == 3)
+            printf("Saturday\n");
+        else if (day == 4)
+            printf("Sunday\n");
+        else if (day == 5)
+            printf("Monday\n");
+        else if (day == 6)
+            printf("Tuesday\n");
+        else if (day == 7)
+            printf("Wednesday\n");
+        else
+            printf("Wednesday\n");
+    }
+    else if (M == 11) {
+        int day = D % 7;
+        if (day == 1)
+            printf("Sunday\n");
+        else if (day == 2)
+            printf("Monday\n");
+        else if (day == 3)
+            printf("Tuesday\n");
+        else if (day == 4)
+            printf("Wednesday\n");
+        else if (day == 5)
+            printf("Thursday\n");
+        else if (day == 6)
+            printf("Friday\n");
+        else if (day == 7)
+            printf("Saturday\n");
+        else
+            printf("Saturday\n");
+    }
+    else if (M == 12) {
+        int day = D % 7;
+        if (day == 1)
+            printf("Tuesday\n");
+        else if (day == 2)
+            printf("Wednesday\n");
+        else if (day == 3)
+            printf("Thursday\n");
+        else if (day == 4)
+            printf("Friday\n");
+        else if (day == 5)
+            printf("Saturday\n");
+        else if (day == 6)
+            printf("Sunday\n");
+        else if (day == 7)
+            printf("Monday\n");
+        else
+            printf("Monday\n");
+    }
+
+    return 0;
+}
